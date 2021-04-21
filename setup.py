2aae6ecab483c766e25f39d36dafefc7f0e1eb14
Mark Stacy <mbstacy@gmail.com>
2021-04-20 22:47:48 -0600

    add templates to setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='emailCULibq',
       version='0.0',
       packages= find_packages(),
-      package_data={"emailCULibq": ["*.j2"]},
+      package_data={"emailCULibq": ['tasks/templates/*.j2', 'emailCULibq/tasks/templates/*.j2']},
       include_package_data=True,
       install_requires=[
           'Jinja2',
