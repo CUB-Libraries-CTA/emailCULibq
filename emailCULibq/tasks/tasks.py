f1180814a622e27dab543c1c485527431e73eab8
Mark Stacy <mbstacy@gmail.com>
2021-04-20 20:18:19 -0600

    initial commit

---
+++ emailCULibq/tasks/tasks.py
@@ -0,0 +1,32 @@
+from celery.task import task
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import os, jinja2
+
+username= os.getenv('SMTP_USERNAME')
+password= os.getenv('SMTP_PASSWORD')
+
+@task()
+def sendEmail(sender_email,receiver_email,subject,template=None,template_data=None):
+    message = MIMEMultipart("alternative")
+    message["Subject"] = subject
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    templateLoader = jinja2.FileSystemLoader(searchpath="./templates/")
+    templateEnv = jinja2.Environment(loader=templateLoader)
+    if template:
+        email_template = templateEnv.get_template(template)
+        text = email_template.render(template_data)
+        part1 = MIMEText(text, template.split('.')[-2])
+    else:
+        text="Error has occured. Template was not found!"
+        part1 = MIMEText(text, "plain")
+    message.attach(part1)
+    # Create secure connection with server and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.colorado.edu", 587, context=context) as server:
+        server.login(username, password)
+        server.sendmail(
+            sender_email, receiver_email, message.as_string()
+        )
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='emailCULibq',
+      version='0.0',
+      packages= find_packages(),
+      install_requires=[
+          'Jinja2',
+      ],
+)
