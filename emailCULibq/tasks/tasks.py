d1faf38a8876139f3f26977977d90cbc8a665f22
Mark Stacy <mbstacy@gmail.com>
2021-04-21 12:59:25 -0600

    changed to package loader

--- emailCULibq/tasks/tasks.py
+++ emailCULibq/tasks/tasks.py
@@ -3,7 +3,7 @@ import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import os, jinja2
-
+from jinja2 import Environment, PackageLoader
 username= os.getenv('EMAIL_HOST_USER')
 password= os.getenv('EMAIL_HOST_PASSWORD')
 
@@ -13,10 +13,13 @@ def sendEmail(sender_email,receiver_email,subject,template=None,template_data=No
     message["Subject"] = subject
     message["From"] = sender_email
     message["To"] = receiver_email
-    templateLoader = jinja2.FileSystemLoader(searchpath="./templates/")
-    templateEnv = jinja2.Environment(loader=templateLoader)
+    env = Environment(loader=PackageLoader('emailCULibq', 'tasks/templates'))
+    # template = env.get_template('index.html')
+    # templateLoader = jinja2.FileSystemLoader(searchpath="./templates/")
+    # templateEnv = jinja2.Environment(loader=templateLoader)
     if template:
-        email_template = templateEnv.get_template("/usr/local/lib/python3.6/site-packages/emailCULibq/tasks/templates/{0}".format(template))
+        email_template = env.get_template(template)
+        # email_template = templateEnv.get_template("/usr/local/lib/python3.6/site-packages/emailCULibq/tasks/templates/{0}".format(template))
         text = email_template.render(template_data)
         part1 = MIMEText(text, template.split('.')[-2])
     else:
