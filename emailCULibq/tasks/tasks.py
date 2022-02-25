1273efc17ea11500d998b68c60ea6236a8143ee2
Mark Stacy <mbstacy@gmail.com>
2022-02-25 13:15:18 -0700

    change to verified from SES

--- emailCULibq/tasks/tasks.py
+++ emailCULibq/tasks/tasks.py
@@ -6,10 +6,10 @@ import os, jinja2
 from jinja2 import Environment, PackageLoader
 username= os.getenv('EMAIL_HOST_USER')
 password= os.getenv('EMAIL_HOST_PASSWORD')
-smtp_host= os.getenv('EMAIL_SMTP_HOST',"smtp.colorado.edu")
+smtp_host= os.getenv('EMAIL_SMTP_HOST')
 
 @task()
-def sendEmail(receiver_email,sender_email,subject,template="default_template.html.j2",template_data={"name":"CU Libraries"}):
+def sendEmail(receiver_email,sender_email,subject,template="default_template.html.j2",template_data={"name":"CU Libraries"},verified_email='libnotify@colorado.edu'):
     """
     CU Libraries email task.
     args: receiver_email,sender_email,subject
@@ -18,8 +18,9 @@ def sendEmail(receiver_email,sender_email,subject,template="default_template.htm
     """
     message = MIMEMultipart("alternative")
     message["Subject"] = subject
-    message["From"] = sender_email
+    message["From"] = verified_email
     message["To"] = receiver_email
+    message["CC"] = sender_email
     env = Environment(loader=PackageLoader('emailCULibq', 'tasks/templates'))
     if template:
         email_template = env.get_template(template)
@@ -32,11 +33,12 @@ def sendEmail(receiver_email,sender_email,subject,template="default_template.htm
     
     # Create secure connection with server and send email
     context = ssl.create_default_context()
+    rcpt= [sender_email] + receiver_email.split(',')
     with smtplib.SMTP(smtp_host, 587) as server:
         server.ehlo()  # Can be omitted
         server.starttls(context=context)
         server.ehlo()  # Can be omitted
         server.login(username, password)
         server.sendmail(
-            sender_email, receiver_email.split(','), message.as_string()
+            verified_email, rcpt, message.as_string()
         )
